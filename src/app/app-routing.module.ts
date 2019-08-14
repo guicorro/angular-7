1b7b3a3e7c0ff3eac238d9093d7e17cfb3791cc7
Guillermo Cortina Rodríguez <guicorro@MacBook-Pro-de-Guillermo.local>
2019-08-14 16:47:04 +0200

    Angular template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+//Components
+import { AboutComponent } from './components/views/about/about.component';
+import { ProjectsComponent } from './components/views/projects/projects.component';
+import { ContactsComponent } from './components/views/contacts/contacts.component';
+import { NotFoundComponent } from './components/views/not-found/not-found.component';
+import { HomeComponent } from './components/views/home/home.component';
+
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'home', component: HomeComponent},
+  {path:'about', component: AboutComponent},
+  {path:'projects', component: ProjectsComponent},
+  {path:'contact', component: ContactsComponent},
+  { path: '**', component: NotFoundComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/views/about/about.component';
+import { ProjectsComponent } from './components/views/projects/projects.component';
+import { ContactsComponent } from './components/views/contacts/contacts.component';
+import { NotFoundComponent } from './components/views/not-found/not-found.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactsComponent,
+    NotFoundComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
